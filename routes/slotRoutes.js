fb8d5f03d1b42cb3880dcd9c6a49d0a8cb5880b6
Srini <srinnivassh@gmail.com>
2026-01-12 00:12:36 +0530

    Update slotRoutes.js

--- routes/slotRoutes.js
+++ routes/slotRoutes.js
@@ -8,8 +8,6 @@ router.get('/slotz', slotController.getSlotsByBillboard);
 
 // Asset routes
 router.get('/assets/:screen_id', slotController.getAssetsByScreen);
-router.post('/track-play', slotController.trackAssetPlay);
-router.get('/asset-logs', slotController.getAssetLogs);
 
 module.exports = router;
 
